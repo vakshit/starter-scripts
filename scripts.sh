15cfabc3bdfea70fd1d5fbefb2defd6b7da493d5
vakshit <akshitv18@gmail.com>
2023-09-05 15:29:11 +0530

    add devops stuff

--- scripts.sh
+++ scripts.sh
@@ -256,6 +256,47 @@ install_discord() {
     prompt -i "Installed Discord"
 }
 
+install_devops() {
+
+    prompt -i "Installing Docker"
+
+    scripts_install ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    scripts_update
+    scripts_install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    prompt -i "Installed Docker"
+
+    prompt -i "Installing AWS CLI"
+
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+    unzip /tmp/awscliv2.zip
+    sudo /tmp/aws/install
+
+    prompt -i "Installed AWS CLI"
+
+    prompt -i "Installing Terraform"
+    scripts_install gnupg software-properties-common
+    wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+    scripts_update
+    scripts_install terraform
+
+    prompt -i "Installed Terraform"
+
+}
+
 install_zsh() {
     prompt -i "Installing ZSH Shell"
 
@@ -299,6 +340,7 @@ fresh_install () {
     install_mailspring
     install_discord
     install_ros
+    install_devops
     install_zsh
 }
 
